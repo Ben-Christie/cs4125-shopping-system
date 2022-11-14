8751c3fadbdc63c22d6a99cb309ba2f1ff134d68
Ben_Christie <ben.christie11@gmail.com>
2022-11-14 22:52:41 +0000

    removal of unused import in IDiscountState

--- Spring/Spring/src/main/java/com/shopmanagementsystem/spring/model/Discount/IDiscountState.java
+++ Spring/Spring/src/main/java/com/shopmanagementsystem/spring/model/Discount/IDiscountState.java
@@ -1,7 +1,5 @@
 package com.shopmanagementsystem.spring.model.Discount;
 
-import com.shopmanagementsystem.spring.Database.DBEntity.DiscountedProduct;
-
 public interface IDiscountState {
     int[] getDiscountRange();
 

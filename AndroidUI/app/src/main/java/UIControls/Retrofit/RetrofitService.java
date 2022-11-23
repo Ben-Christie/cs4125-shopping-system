a0038ff219c56037a99497f2cefb7ae5aade90d4
Aaron Meade <64416980+aaron020@users.noreply.github.com>
2022-11-23 16:32:14 +0000

    Retrofit revert

--- AndroidUI/app/src/main/java/UIControls/Retrofit/RetrofitService.java
+++ AndroidUI/app/src/main/java/UIControls/Retrofit/RetrofitService.java
@@ -15,7 +15,7 @@ public class RetrofitService {
     private void initializeRetrofit() {
         retrofit = new Retrofit.Builder()
                                 //IPv4 Address:Port the server is running on
-                .baseUrl("http://192.168.15.185:8080")
+                .baseUrl("http://localhost:port")
                 .addConverterFactory(GsonConverterFactory.create(new Gson()))
                 .build();
     }

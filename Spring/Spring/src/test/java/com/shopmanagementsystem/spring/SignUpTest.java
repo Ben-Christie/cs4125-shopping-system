d588f00fd509645b5322277e26cef97803538ea6
Max-HTML <57290732+Max-HTML@users.noreply.github.com>
2022-12-02 19:44:16 +0000

    Update SignUpTest.java

--- Spring/Spring/src/test/java/com/shopmanagementsystem/spring/SignUpTest.java
+++ Spring/Spring/src/test/java/com/shopmanagementsystem/spring/SignUpTest.java
@@ -8,13 +8,13 @@ class SignUpTest {
 
     @Test
     void checkEmailAvailableTest() {
-    var checkEmailisAvailable = new checkEmailAvailable();
+    checkEmailAvailable checkEmailisAvailable = new checkEmailAvailable();
     assertEquals(true || false, checkEmailisAvailable("test@test.com"));
     }
 
     @Test
     void createUserTest() {
-        var createUserTest = new createUser();
+        createUser createUserTest = new createUser();
         assertEquals(true || false, createUserTest("test@test.com"));
     }
 }
